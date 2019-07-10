c5c31c4b4ce7a7fc0cd7e60efa24aebe72318562
ArawigaH <52554239+ArawigaH@users.noreply.github.com>
2019-07-11 00:25:05 +0900

    Update dicegame.c

--- dicegame.c
+++ dicegame.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 int main(){
 
-   int d1,d2;
+   int d1,d2,i;
    int result;
+   unsigned int t;
+    
+    t = time(NULL);
+    srand(t);
 
 printf("Rolling the dice...\n");
 
 d1 = rand() % 6 + 1;
+   for(i=0;i<rand()%6+1;i++){
+    }
 
 printf("Die 1: %d\n",d1);
 
